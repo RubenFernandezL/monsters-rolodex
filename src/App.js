b721050d4c0e7db000bf2479f801f6fd5f3cbec7
Rubén Fernández Lopesino <ruben.fernandezlopesino@ust.com>
2022-07-27 17:43:05 +0200

    First render with API data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      query: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((apiUsers) =>
+        this.setState(() => {
+          return { users: apiUsers };
+        })
+      );
+  }
+
+  search = (event) => {
+    const query = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { query };
+    });
+  };
+
+  render() {
+    const { users, query } = this.state;
+    const { search } = this;
+
+    const filteredUsers = users.filter((user) =>
+      user.name.toLocaleLowerCase().includes(query)
+    );
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search user"
+          onChange={search}
+        />
+        {filteredUsers?.map((user) => (
+          <h1 key={user.id}>{user.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
